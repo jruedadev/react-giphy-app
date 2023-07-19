816e42361ac61620c1bd142fd59bb463bc74fa6f
Johan Roberto Rueda <librajohan2000@gmail.com>
2023-07-18 20:48:16 -0500

    Deploy de prueba

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,31 @@
 /* eslint-env node */
 
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true, // Agregamos "node" para reconocer el entorno Node.js
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 2021, // Cambiamos a 2021 para admitir la última versión de ECMAScript
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Cambiamos a "detect" para que ESLint detecte la versión de React automáticamente
+    },
+  },
+  plugins: ['react', 'react-hooks', 'react-refresh'], // Agregamos los plugins de react y react-hooks
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
